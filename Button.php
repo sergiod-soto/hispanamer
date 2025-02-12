c0461da9eb4566778be6dbcefd88d1cb19d63e79
sergiod <sergio.dominguez.ingeniero@gmail.com>
2025-02-12 15:12:12 +0100

    commit

--- Button.php
+++ Button.php
@@ -10,12 +10,12 @@ class Button extends Elemento implements IEditable, IRenderizable
 
     public $funcion;
     public $text;
-
     private $programa;
 
     public function __construction($id, $visible = true, $modo, $text = "", $funcion = null, $padre, $programa, $estilo)
     {
-        $html = "";
+        $html = "<button type='button'>$text</button>";
+
         // Llamamos al constructor de la clase Elemento
         parent::__construct($id, $visible, $modo, $padre, $programa, $html, $estilo);
 
@@ -26,43 +26,52 @@ class Button extends Elemento implements IEditable, IRenderizable
     }
 
     /*
-        TODO
+        cambia el estilo a editable
     */
     public function setEditableOn()
     {
-
+        // TODO
     }
 
     /*
-        TODO
+        cambia el estilo a NO editable
     */
     public function setEditableOff()
     {
-
+        // TODO
     }
 
     /*
-        TODO
+        vuelve al elemento invisible
     */
     function hide()
     {
-
+        // TODO
     }
 
     /*
-        TODO
+        vuelve al elemento visible
     */
     function show()
     {
-
+        // TODO
     }
 
     /*
-        TODO
+        se cambia el texto que aparece en el boton
+    */
+    function cambiarText($text)
+    {
+        $this->text = $text;
+        $this->html = "<button type='button'>$text</button>";
+    }
+
+    /*
+        retorna el html asociado
     */
     function Renderizar()
     {
-        return "TODO";
+        return $this->html;
     }
 
 }
