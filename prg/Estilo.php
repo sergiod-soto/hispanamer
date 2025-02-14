c163a8aa8b1f55b51875b32ee0989eac541a2028
sergiod <sergio.dominguez.ingeniero@gmail.com>
2025-02-14 16:57:50 +0100

    commit

--- prg/Estilo.php
+++ prg/Estilo.php
@@ -14,27 +14,31 @@ class Estilo
         facil, rapido y comodo.
         para otras propiedades, introducir las etiquetas manualmente.
     */
-    public $fontObject = new Font; // font como objeto
+    public $fontObject = new Font;                      // font como objeto
     public $font = $this->fontObject->toString();       // font como string
     public $colorLetra = Color::rgb(0, 0, 0);
     public $colorFondo = Color::rgba(255, 255, 255, 0);
-    public $grosorBorde = "2px";
-    public $lineaBorde = "none";
-    public $colorBorde = Color::rgb(0, 0, 0);
-    public $margenIzq = "20px";
-    public $margenDer = "20px";
-    public $margenSuperior = "15px";
-    public $margenInferior = "15px";
+    public $estiloBordeIzq;
+    public $estiloBordeDer;
+    public $estiloBordeSuperior;
+    public $estiloBordeInferior;
+    public $lineaBorde;
+    public $colorBorde;
+    public $margenIzq;
+    public $margenDer;
+    public $margenSuperior;
+    public $margenInferior;
     public $anchura;
     public $altura;
     public $anchuraMaxima;
     public $alturaMaxima;
-    public $alineamiento = "center";
+    public $alineamiento = "left";
     public $display;
 
     public function __construct(array $propiedades = [])
     {
-        $css = "";
+        $this->css = "";
+        $this->activo = true;
 
         foreach ($propiedades as $propiedad => $valor) {
             if (property_exists($this, $propiedad)) {
@@ -75,21 +79,48 @@ class Estilo
 
         $css .= $this->fontObject->toString();  // font como string
 
-        $css .= $this->colorLetra == null ? Color::rgb(0, 0, 0) . " " : $this->colorLetra . " ";
-        $css .= $this->colorFondo == null ? Color::rgb(255, 255, 255) . " " : $this->colorFondo . " ";
-        $css .= $this->grosorBorde == null ? "" : "";
-        $css .= $this->lineaBorde == null ? "" : "border-style: groove; ";
-        $css .= $this->colorBorde == null ? "" : "";
-        $css .= $this->margenIzq == null ? "" : "";
-        $css .= $this->margenDer == null ? "" : "";
-        $css .= $this->margenSuperior == null ? "" : "";
-        $css .= $this->margenInferior == null ? "" : "";
-        $css .= $this->anchura == null ? "" : "";
-        $css .= $this->altura == null ? "" : "";
-        $css .= $this->anchuraMaxima == null ? "" : "";
-        $css .= $this->alturaMaxima == null ? "" : "";
-        $css .= $this->alineamiento == null ? "" : "";
-        $css .= $this->display == null ? "" : "";
+        $css .= "color:" . $this->colorLetra . "; ";
+        $css .= "color:" . $this->colorFondo . "; ";
+        $css .= $this->estiloBordeIzq == null
+            ? ""
+            : "border-left-style:" . $this->estiloBordeIzq . "; ";
+        $css .= $this->estiloBordeDer == null
+            ? ""
+            : "border-right-style:" . $this->estiloBordeDer . "; ";
+        $css .= $this->estiloBordeSuperior == null
+            ? ""
+            : "border-top-style:" . $this->estiloBordeSuperior . "; ";
+        $css .= $this->estiloBordeInferior == null
+            ? ""
+            : "border-botton-style:" . $this->estiloBordeInferior . "; ";
+        $css .= $this->margenIzq == null
+            ? ""
+            : "margin-left:" . $this->margenIzq . "; ";
+        $css .= $this->margenDer == null
+            ? ""
+            : "margin-right:" . $this->margenDer . "; ";
+        $css .= $this->margenSuperior == null
+            ? ""
+            : "margin-top:" . $this->margenSuperior . "; ";
+        $css .= $this->margenInferior == null
+            ? ""
+            : "margin-bottom:" . $this->margenInferior . "; ";
+        $css .= $this->anchura == null
+            ? ""
+            : "";
+        $css .= $this->altura == null
+            ? ""
+            : "";
+        $css .= $this->anchuraMaxima == null
+            ? ""
+            : "";
+        $css .= $this->alturaMaxima == null
+            ? ""
+            : "";
+        $css .= " ;"; // alineamiento
+        $css .= $this->display == null
+            ? ""
+            : "";
 
         $css .= "}";
         $css .= "</style>";
