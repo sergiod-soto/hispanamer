70a17009877211d0d1f67d1d5b10e728b91640e2
sergiod <sergio.dominguez.ingeniero@gmail.com>
2025-05-12 13:53:34 +0200

    actualizada cabecera. funciona con nueva Seccion

--- php/prefabricados/Cabecera.php
+++ php/prefabricados/Cabecera.php
@@ -11,21 +11,7 @@ include_once "php/Includes.php";
 
 class Cabecera
 {
-
-    public static function createFunctions($fAtras, $fNuevo, $fEditar, $fBorrar, $fPDF, $fExcel, $fGuardar, $fCancelar)
-    {
-        return [
-            "atras" => $fAtras,
-            "nuevo" => $fNuevo,
-            "editar" => $fEditar,
-            "borrar" => $fBorrar,
-            "pdf" => $fPDF,
-            "excel" => $fExcel,
-            "guardar" => $fGuardar,
-            "cancelar" => $fCancelar,
-        ];
-    }
-    public static function createCabecera($nombre, $version, $ejercicioFiscal, $functions)
+    public static function createCabecera($nombrePrograma, $nombre, $version, $ejercicioFiscal, $funciones)
     {
         $cabecera = Seccion::crear("cabeceraBase", "cabecera", false);
 
@@ -58,15 +44,15 @@ class Cabecera
         ]);
 
         $superior1->add([
-            Texto::crear("", "textoCabecera", "Sesion iniciada: {\$nombre}"),
-            Texto::crear("nombre_programa", "textoCabecera", "{\$nombrePrograma}")
+            Texto::crear("", "textoCabecera", "Sesion iniciada: {$nombre}"),
+            Texto::crear("nombre_programa", "textoCabecera", "{$nombrePrograma}")
         ]);
         $superior2->add([
             Texto::crear("", "textoCabecera", Programa::fechaActual()),
         ]);
         $superior3->add([
-            Texto::crear("textoVersion", "textoCabecera", "Versión: 250305-000107"),
-            Texto::crear("textoEjercicioFiscal", "textoCabecera", "Ejercicio fiscal: {\$año}")
+            Texto::crear("textoVersion", "textoCabecera", "Versión: {$version}"),
+            Texto::crear("textoEjercicioFiscal", "textoCabecera", "Ejercicio fiscal: {$ejercicioFiscal}")
         ]);
 
         $inferior->add([
@@ -75,15 +61,15 @@ class Cabecera
         ]);
 
         $inferior1->add([
-            Button::crear("botonNuevo", "", iconoNuevo, "Nuevo", ""),
-            Button::crear("botonEditar", "", iconoEditar, "Editar", ""),
-            Button::crear("botonEliminar", "", iconoEliminar, "Eliminar", ""),
-            Button::crear("botonPDF", "", iconoPDF, "PDF", ""),
-            Button::crear("botonEXCEL", "", iconoEXCEL, "EXCEL", ""),
+            Button::crear("botonNuevo", "", iconoNuevo, "Nuevo", $funciones["nuevo"]),
+            Button::crear("botonEditar", "", iconoEditar, "Editar", $funciones["editar"]),
+            Button::crear("botonEliminar", "", iconoEliminar, "Eliminar", $funciones["eliminar"]),
+            Button::crear("botonPDF", "", iconoPDF, "PDF", $funciones["pdf"]),
+            Button::crear("botonEXCEL", "", iconoEXCEL, "EXCEL", $funciones["excel"]),
         ]);
 
         $inferior2->add([
-            Button::crear("botonImprimir", "", iconoImpresora, "Imprimir", "")
+            Button::crear("botonImprimir", "", iconoImpresora, "Imprimir", $funciones["imprimir"])
         ]);
 
         return $cabecera;
